48a984530e156ee1bdd69b58157a1e12217adf3b
Pranzal Aryan <77982296+pranzalaryan240830@users.noreply.github.com>
2022-02-08 18:04:01 +0530

    Day_2_Servlets | Pranzal Aryan

---
+++ GenX_Servlet_and_JSP/src/main/java/add/day_1_JavaServlet.java
@@ -0,0 +1,17 @@
+package add;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class day_1_JavaServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int sum = i+j;
+		PrintWriter out = res.getWriter();
+		out.println("Sum is: "+sum);
+	}
+}
